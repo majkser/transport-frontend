db28f0e459a91a349862c907829b321c330cea3b
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-07 01:18:35 +0100

    feat: added bg image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 import Typography from "@/components/Typography/typography";
+import backgroundImage from "@/public/bgImage.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <Typography variant="h1">TEXT</Typography>
+    <div
+      className="relative -mt-16 pt-24 h-screen bg-cover bg-center flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${backgroundImage.src})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div>
+        <Typography variant="h1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          voluptate nisi minima odit ex non dolorum et doloremque suscipit hic
+          quis, sequi quam repellendus voluptatibus vel provident minus natus
+        </Typography>
+        <Typography variant="h1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          voluptate nisi minima odit ex non dolorum et doloremque suscipit hic
+          quis, sequi quam repellendus voluptatibus vel provident minus natus
+        </Typography>
+        <Typography variant="h1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          voluptate nisi minima odit ex non dolorum et doloremque suscipit hic
+          quis, sequi quam repellendus voluptatibus vel provident minus natus
+        </Typography>
+      </div>
     </div>
   );
 }
