8500125fd58c2f691757554f5480116211e222f3
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-10 17:23:45 +0100

    fix: added keys in cards components

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -12,7 +12,7 @@ export default function Card({ title, text, image }: CardProps) {
   return (
     <CardContainer>
       <CardBody className="h-[500px] w-[300px] relative rounded-2xl text-white bg-black overflow-hidden hover:shadow-2xl shadow-red-600 transition-all duration-300">
-        <CardItem className="h-3/4 w-full relative">
+        <CardItem key="image" className="h-3/4 w-full relative">
           <Image
             src={image}
             alt="card image"
@@ -21,10 +21,10 @@ export default function Card({ title, text, image }: CardProps) {
             className="object-cover"
           />
         </CardItem>
-        <CardItem>
+        <CardItem key="title">
           <Typography variant="h3">{title}</Typography>
         </CardItem>
-        <CardItem>
+        <CardItem key="text">
           <Typography variant="p">{text}</Typography>
         </CardItem>
       </CardBody>
