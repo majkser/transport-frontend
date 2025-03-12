18d8e156aa84a1eb35f5953f260b1e045b958010
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-12 20:16:28 +0100

    fix: moved section to the main page

--- components/Cards/cards.tsx
+++ components/Cards/cards.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card/card";
 
 export default function Cards() {
   return (
-    <section className="max-width-[1440px] m-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <>
       <Card
         title="Card Title"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
@@ -19,6 +19,6 @@ export default function Cards() {
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
         image={backgroundImage}
       ></Card>
-    </section>
+    </>
   );
 }
