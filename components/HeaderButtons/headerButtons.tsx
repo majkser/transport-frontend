b4d9cf0c4d0db5e7693275fe0dd2a6379efd3954
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-12 20:15:25 +0100

    fix&feat: added proper underline eff with event listner

--- components/HeaderButtons/headerButtons.tsx
+++ components/HeaderButtons/headerButtons.tsx
@@ -1,16 +1,47 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
 import Typography from "@/components/Typography/typography";
 
 export default function HeaderButtons() {
-  const [active, setActive] = useState<string>("");
+  const [active, setActive] = useState<string>("home");
 
   function handleClick(buttonName: string) {
-    setActive(buttonName);
+    const section = document.getElementById(buttonName);
+    if (section) {
+      const height = section.getBoundingClientRect().height;
+      const middleOffset =
+        section.offsetTop - window.innerHeight / 2 + height / 2;
+
+      window.scrollTo({ top: middleOffset, behavior: "smooth" });
+    }
   }
+
+  function handleScroll() {
+    const home = document.getElementById("home");
+    const contact = document.getElementById("contact");
+
+    if (home && contact) {
+      const homeTop = home.getBoundingClientRect().top;
+      const contactTop = contact.getBoundingClientRect().top;
+
+      if (homeTop <= 0 && contactTop > 0) {
+        setActive("home");
+      }
+      if (contactTop <= 500) {
+        setActive("contact");
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <nav className="flex items-center gap-8">
@@ -32,7 +63,7 @@ export default function HeaderButtons() {
           active === "contact" && "underline"
         )}
       >
-        <Typography variant="p">Contact</Typography>
+        <Typography variant="p">Contact us</Typography>
       </Button>
       <Button
         onClick={() => handleClick("faq")}
